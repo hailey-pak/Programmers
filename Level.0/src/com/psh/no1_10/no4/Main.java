08551f055768bbbeb33b0e01890f6d5d7125c15e
hailey-pak <haileypak.me@gmail.com>
2022-10-29 01:59:30 +0900

    코딩테스트 입문 > 몫 구하기

---
+++ Level.0/src/com/psh/no1_10/no4/Main.java
@@ -0,0 +1,15 @@
+package com.psh.no1_10.no4;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int answer = solution(sc.nextInt(), sc.nextInt());
+        System.out.println(answer);
+    }
+
+    static int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+}
