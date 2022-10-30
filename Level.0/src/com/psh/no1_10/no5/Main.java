d5183affd597a2843292244d8b229138783794b7
hailey-pak <haileypak.me@gmail.com>
2022-10-30 20:45:03 +0900

    코딩테스트 입문 > 두 수의 나눗셈

---
+++ Level.0/src/com/psh/no1_10/no5/Main.java
@@ -0,0 +1,16 @@
+package com.psh.no1_10.no5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int answer = solution(sc.nextInt(), sc.nextInt());
+        System.out.println(answer);
+    }
+
+    static int solution(int num1, int num2) {
+        double d = (double) num1 / num2;
+        return (int) (d * 1000);
+    }
+}
