4b79e5141fce17562b11896c12c0b5b726d2611d
hailey-pak <haileypak.me@gmail.com>
2022-10-30 22:12:25 +0900

    코딩테스트 입문 > 분수의 덧셈

---
+++ Level.0/src/com/psh/no1_10/no7/Main.java
@@ -0,0 +1,29 @@
+package com.psh.no1_10.no7;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] answer = solution(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt());
+        System.out.println(Arrays.toString(answer));
+    }
+
+    static int[] solution(int denum1, int num1, int denum2, int num2) {
+        int fraction = denum1 * num2 + denum2 * num1;
+        int numerator = num1 * num2;
+        int mod = gcd(fraction, numerator);
+        return new int[]{fraction/mod, numerator/mod};
+    }
+
+    static int gcd(int a, int b) {
+        if (a < b) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        if (b == 0) return a;
+        return gcd(b , a % b);
+    }
+}
