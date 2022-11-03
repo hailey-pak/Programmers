2f29eefd02bc2cb8d879d4884bc21d57c7598c92
haileypak <haileypak.me@gmail.com>
2022-11-04 08:36:31 +0900

    코딩테스트 입문 > 특정 문자 제거하기

---
+++ Level.0/src/com/psh/no21_30/no25/Main.java
@@ -0,0 +1,18 @@
+package com.psh.no21_30.no25;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String my_string = br.readLine();
+        String letter = br.readLine();
+        System.out.print(solution(my_string, letter));
+    }
+
+    static String solution(String my_string, String letter) {
+        return my_string.replaceAll(letter, "");
+    }
+}
