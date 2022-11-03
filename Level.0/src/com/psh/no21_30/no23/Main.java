6377130a7f8a2d283c59ba196615c2548e819a08
hailey-pak <haileypak.me@gmail.com>
2022-11-03 22:13:35 +0900

    코딩테스트 입문 > 짝수 홀수 개수

---
+++ Level.0/src/com/psh/no21_30/no23/Main.java
@@ -0,0 +1,23 @@
+package com.psh.no21_30.no23;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int[] num_list = Arrays.stream(str).mapToInt(Integer::parseInt).toArray();
+        System.out.println(Arrays.toString(solution(num_list)));
+    }
+
+    static int[] solution(int[] num_list) {
+        int odd = 0, even = 0;
+        for (int n : num_list)
+            if (n % 2 == 0) even++;
+            else odd++;
+        return new int[]{even, odd};
+    }
+}
