168ed421277acdf044477fb4d890987d286a3ca6
haileypak <haileypak.me@gmail.com>
2022-11-03 21:24:27 +0900

    코딩테스트 입문 > 배열 뒤집기

---
+++ Level.0/src/com/psh/no11_20/no20/Main.java
@@ -0,0 +1,27 @@
+package com.psh.no11_20.no20;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int[] numbers = new int[str.length];
+        for (int i = 0; i < str.length; i++) {
+            numbers[i] = Integer.parseInt(str[i]);
+        }
+        System.out.print(Arrays.toString(solution(numbers)));
+
+    }
+
+    static int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = num_list[num_list.length-1-i];
+        }
+        return answer;
+    }
+}
