232c899df6fd2f1b945b5a68e7aa648b0aeb8616
hailey-pak <haileypak.me@gmail.com>
2022-11-03 22:17:36 +0900

    코딩테스트 입문 > 문자 반복 출력하기

---
+++ Level.0/src/com/psh/no21_30/no24/Main.java
@@ -0,0 +1,23 @@
+package com.psh.no21_30.no24;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String my_string = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(solution(my_string, n));
+    }
+
+    static String solution(String my_string, int n) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < my_string.length(); i++) {
+            String s = String.valueOf(my_string.charAt(i));
+            answer.append(s.repeat(n));
+        }
+        return answer.toString();
+    }
+}
