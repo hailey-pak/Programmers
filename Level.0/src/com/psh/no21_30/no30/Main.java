399c271c0d22c05d07e34c21c8b06a98e53d994b
hailey-pak <haileypak.me@gmail.com>
2022-11-05 23:30:14 +0900

    코딩테스트 입문 > 외계행성의 나이

---
+++ Level.0/src/com/psh/no21_30/no30/Main.java
@@ -0,0 +1,20 @@
+package com.psh.no21_30.no30;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int age = new Scanner(System.in).nextInt();
+        System.out.print(solution(age));
+    }
+
+    static String solution(int age) {
+        String[] str = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+        StringBuilder sb = new StringBuilder();
+        while (age > 0) {
+            sb.append(str[age % 10]);
+            age /= 10;
+        }
+        return sb.reverse().toString();
+    }
+}
