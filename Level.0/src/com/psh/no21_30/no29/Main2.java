71ea2d1d890c95d5541457719a26b1c92644a087
hailey-pak <haileypak.me@gmail.com>
2022-11-05 23:14:36 +0900

    코딩테스트 입문 > 배열 자르기 > Arrays.copyOfRange

---
+++ Level.0/src/com/psh/no21_30/no29/Main2.java
@@ -0,0 +1,23 @@
+package com.psh.no21_30.no29;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+
+        int[] numbers = Arrays.stream(str).mapToInt(Integer::parseInt).toArray();
+        int num1 = Integer.parseInt(br.readLine());
+        int num2 = Integer.parseInt(br.readLine());
+
+        System.out.println(Arrays.toString(solution(numbers, num1, num2)));
+    }
+
+    static int[] solution(int[] numbers, int num1, int num2) {
+        return Arrays.copyOfRange(numbers, num1, num2 + 1);
+    }
+}
