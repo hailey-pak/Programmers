fbf1691b5bab825757bdd79ff92df631f21cf793
hailey-pak <haileypak.me@gmail.com>
2022-11-06 21:24:41 +0900

    코딩테스트 입문 > 가위 바위 보

---
+++ Level.0/src/com/psh/no31_40/no35/Main.java
@@ -0,0 +1,30 @@
+package com.psh.no31_40.no35;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String rsp = br.readLine();
+        System.out.print(solution(rsp));
+    }
+
+    static String solution(String rsp) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < rsp.length(); i++) {
+            switch (rsp.charAt(i)){
+                case '0':
+                    answer.append('5');
+                    break;
+                case '2':
+                    answer.append('0');
+                    break;
+                default:
+                    answer.append('2');
+            }
+        }
+        return answer.toString();
+    }
+}
