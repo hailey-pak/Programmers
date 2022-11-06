792daf56d63d0e1e58f61f70bc2ef23bc4bf4e2d
hailey-pak <haileypak.me@gmail.com>
2022-11-06 21:51:04 +0900

    코딩테스트 입문 > 구슬을 나누는 경우의 수

---
+++ Level.0/src/com/psh/no31_40/no36/Main.java
@@ -0,0 +1,21 @@
+package com.psh.no31_40.no36;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int balls = sc.nextInt();
+        int share = sc.nextInt();
+        System.out.print(solution(balls, share));
+    }
+
+    static int solution(int balls, int share) {
+        return combi(balls, share);
+    }
+
+    static int combi(int n, int r) {
+        if (r == 0 || r == n) return 1;
+        return combi(n-1, r-1) + combi(n-1, r);
+    }
+}
