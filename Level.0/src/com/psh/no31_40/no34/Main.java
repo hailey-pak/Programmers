61fbd553448e0432e4571490aa200fb17d0839c9
hailey-pak <haileypak.me@gmail.com>
2022-11-06 20:44:33 +0900

    코딩테스트 입문 > 모스부호 (1)

---
+++ Level.0/src/com/psh/no31_40/no34/Main.java
@@ -0,0 +1,27 @@
+package com.psh.no31_40.no34;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    private static final String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..",
+            "--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String letter = br.readLine();
+        System.out.print(solution(letter));
+    }
+
+    static String solution(String letter) {
+        StringBuilder sb = new StringBuilder();
+        String[] str = letter.split(" ");
+        for (String s : str) {
+            int idx = Arrays.asList(morse).indexOf(s);
+            sb.append(Character.toString(idx + 97));
+        }
+        return sb.toString();
+    }
+}
