9b53557486b5a356965c114a4db297b5324de46a
haileypak <haileypak.me@gmail.com>
2022-11-07 09:30:07 +0900

    코딩테스트 입문 > 공 던지기

---
+++ Level.0/src/com/psh/no31_40/no39/Main.java
@@ -0,0 +1,24 @@
+package com.psh.no31_40.no39;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+
+        int[] numbers = Arrays.stream(str).mapToInt(Integer::parseInt).toArray();
+        int k = Integer.parseInt(br.readLine());
+        System.out.println(solution(numbers, k));
+    }
+
+    static int solution(int[] numbers, int k) {
+        int n = 2 * k - 1;
+        int idx = numbers.length > n ? n: n % numbers.length;
+        idx = idx - 1 < 0 ? numbers.length - 1 : idx - 1;
+        return numbers[idx];
+    }
+}
