41e27d54387660b743ac6766dcb6a30102cca054
haileypak <haileypak.me@gmail.com>
2022-11-08 09:36:34 +0900

    코딩테스트 입문 > 합성수 찾기 > IntStream 이용

---
+++ Level.0/src/com/psh/no41_50/no42/Main2.java
@@ -0,0 +1,16 @@
+package com.psh.no41_50.no42;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        System.out.print(solution(n));
+    }
+
+    static int solution(int n) {
+        return (int) IntStream.rangeClosed(1, n).filter(i ->
+                (int) IntStream.rangeClosed(1, i).filter(i2 -> i % i2 == 0).count() > 2).count();
+    }
+}
