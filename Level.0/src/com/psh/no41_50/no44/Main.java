0b0a9d1802c11632b48efb13276f99069be4d9ab
haileypak <haileypak.me@gmail.com>
2022-11-08 10:05:10 +0900

    코딩테스트 입문 > 팩토리얼

---
+++ Level.0/src/com/psh/no41_50/no44/Main.java
@@ -0,0 +1,29 @@
+package com.psh.no41_50.no44;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        System.out.print(solution(n));
+    }
+
+    static int solution(int n) {
+        int idx = 1;
+        while (true) {
+            int val = factorial(idx);
+            if (val == n) return idx;
+            if (val > n) return idx-1;
+            idx++;
+        }
+    }
+
+    static int factorial (int num) {
+        if (num == 1)
+            return 1;
+
+        return num * factorial ( num - 1 );
+    }
+
+}
+
