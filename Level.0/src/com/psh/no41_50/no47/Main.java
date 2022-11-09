ade86ecf2fd417c0ea6ba7774b4e44b378331f06
haileypak <haileypak.me@gmail.com>
2022-11-09 09:41:44 +0900

    코딩테스트 입문 > 문자열 정렬하기

---
+++ Level.0/src/com/psh/no41_50/no47/Main.java
@@ -0,0 +1,20 @@
+package com.psh.no41_50.no47;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String my_string = new Scanner(System.in).nextLine();
+        System.out.println(solution(my_string));
+    }
+
+    static int solution(String my_string) {
+        int sum = 0;
+        for (int i = 0; i < my_string.length(); i++) {
+            if ( Character.isDigit(my_string.charAt(i))) {
+                sum += my_string.charAt(i) - '0';
+            }
+        }
+        return sum;
+    }
+}
