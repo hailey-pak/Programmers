e9c36ae6bcf87839ee3bf7191e9fd6e6b0f394ab
haileypak <haileypak.me@gmail.com>
2022-11-10 09:35:56 +0900

    코딩테스트 입문 > 삼각형의 완성조건 (1)

---
+++ Level.0/src/com/psh/no51_60/no52/Main.java
@@ -0,0 +1,24 @@
+package com.psh.no51_60.no52;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int[] sides = Arrays.stream(str).mapToInt(Integer::parseInt).toArray();
+        System.out.print(solution(sides));
+    }
+
+    static int solution(int[] sides) {
+//        int max = Arrays.stream(sides).max().getAsInt();
+//        int sum = Arrays.stream(sides).sum();
+//        return sum - max > max ? 1 : 2;
+        int max = Arrays.stream(sides).max().getAsInt();
+        int sum = Arrays.stream(sides).sum();
+        return sum - max > max ? 1 : 2;
+    }
+}
