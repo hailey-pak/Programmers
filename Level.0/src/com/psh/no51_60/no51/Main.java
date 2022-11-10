651c83bf8bc1b13c69460c9ca38a668b4794b2bf
haileypak <haileypak.me@gmail.com>
2022-11-10 09:25:57 +0900

    코딩테스트 입문 > 중복된 문자 제거

---
+++ Level.0/src/com/psh/no51_60/no51/Main.java
@@ -0,0 +1,25 @@
+package com.psh.no51_60.no51;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String my_string = br.readLine();
+        System.out.println(solution(my_string));
+    }
+
+    static String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string.charAt(0));
+        for (int i = 1; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if (!sb.toString().contains(c+"")) {
+                sb.append(my_string.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
