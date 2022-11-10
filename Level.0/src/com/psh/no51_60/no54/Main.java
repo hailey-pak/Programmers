3c2f3dd306510b68331890cd351643814f1b0b0f
haileypak <haileypak.me@gmail.com>
2022-11-11 08:58:37 +0900

    코딩테스트 입문 > 369게임

---
+++ Level.0/src/com/psh/no51_60/no54/Main.java
@@ -0,0 +1,21 @@
+package com.psh.no51_60.no54;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int order = new Scanner(System.in).nextInt();
+        System.out.print(solution(order));
+    }
+
+    static int solution(int order) {
+        int answer = 0;
+        String str = String.valueOf(order);
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '3' || str.charAt(i) == '6' || str.charAt(i) == '9') {
+                answer ++ ;
+            }
+        }
+        return answer;
+    }
+}
