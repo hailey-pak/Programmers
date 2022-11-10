fd0bb5a1f4d5cf83394b4930c3f2903b441c6577
haileypak <haileypak.me@gmail.com>
2022-11-10 09:10:41 +0900

    코딩테스트 입문 > 배열 원소의 길이

---
+++ Level.0/src/com/psh/no41_50/no50/Main.java
@@ -0,0 +1,22 @@
+package com.psh.no41_50.no50;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] strlist = br.readLine().split(" ");
+        System.out.println(Arrays.toString(solution(strlist)));
+    }
+
+    static int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        for (int i = 0; i < strlist.length; i++) {
+            answer[i] = strlist[i].length();
+        }
+        return answer;
+    }
+}
