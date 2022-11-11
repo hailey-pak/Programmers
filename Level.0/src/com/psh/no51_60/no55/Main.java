d605098e117d9b66f42d27d84e6451a7dc34e820
haileypak <haileypak.me@gmail.com>
2022-11-11 09:04:56 +0900

    코딩테스트 입문 > 암호해독

---
+++ Level.0/src/com/psh/no51_60/no55/Main.java
@@ -0,0 +1,22 @@
+package com.psh.no51_60.no55;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String cipher = br.readLine();
+        int code = Integer.parseInt(br.readLine());
+        System.out.print(solution(cipher, code));
+    }
+
+    static String solution(String cipher, int code) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = code; i <= cipher.length(); i += code) {
+            sb.append(cipher.charAt(i-1));
+        }
+        return sb.toString();
+    }
+}
