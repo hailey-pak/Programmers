afd48a9360925f4c76560710dc9e61ffb52ecc76
haileypak <haileypak.me@gmail.com>
2022-11-11 09:09:43 +0900

    코딩테스트 입문 > 대문자와 소문자

---
+++ Level.0/src/com/psh/no51_60/no56/Main.java
@@ -0,0 +1,19 @@
+package com.psh.no51_60.no56;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String my_string = new Scanner(System.in).nextLine();
+        System.out.println(solution(my_string));
+    }
+
+    static String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : my_string.toCharArray()) {
+            sb.append(Character.isUpperCase(c) ? Character.toLowerCase(c) : Character.toUpperCase(c));
+        }
+        return sb.toString();
+    }
+
+}
