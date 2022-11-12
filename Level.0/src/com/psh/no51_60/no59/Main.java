a2cefb60ad02dcadcc5d8847293cfaf83546055d
hailey-pak <haileypak.me@gmail.com>
2022-11-12 13:49:42 +0900

    코딩테스트 입문 > 한 번만 등장한 문자

---
+++ Level.0/src/com/psh/no51_60/no59/Main.java
@@ -0,0 +1,34 @@
+package com.psh.no51_60.no59;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(solution(s));
+    }
+
+    static String solution(String s) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            String val = s.charAt(i)+"";
+            if (map.containsKey(val)) {
+                map.put(val, map.get(val)+1);
+            } else {
+                map.put(val, 1);
+            }
+        }
+
+        List<String> list = new ArrayList<>();
+        for (String key : map.keySet()) {
+            if (map.get(key)==1) list.add(key);
+        }
+        Collections.sort(list);
+
+        return String.join("", list);
+    }
+}
