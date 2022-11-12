9f013617c24c50a38a362a22797fc6054db233de
hailey-pak <haileypak.me@gmail.com>
2022-11-12 15:32:42 +0900

    코딩테스트 입문 > 약수 구하기

---
+++ Level.0/src/com/psh/no51_60/no60/Main.java
@@ -0,0 +1,23 @@
+package com.psh.no51_60.no60;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        System.out.print(Arrays.toString(solution(n)));
+    }
+
+    static int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                list.add(i);
+            }
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
