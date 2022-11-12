80c6b0bcc56ea3d1f149753937b450403f3c6025
hailey-pak <haileypak.me@gmail.com>
2022-11-12 12:01:35 +0900

    코딩테스트 입문 > 영어가 싫어요

---
+++ Level.0/src/com/psh/no51_60/no57/Main.java
@@ -0,0 +1,42 @@
+package com.psh.no51_60.no57;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String numbers = br.readLine();
+        System.out.print(solution(numbers));
+    }
+
+    static long solution(String numbers) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("zero", 0);
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+        map.put("four", 4);
+        map.put("five", 5);
+        map.put("six", 6);
+        map.put("seven", 7);
+        map.put("eight", 8);
+        map.put("nine", 9);
+
+        StringBuilder sb = new StringBuilder();
+        int b = 0, e = 2;
+        while (e <= numbers.length()) {
+            String s = numbers.substring(b, e);
+            if (map.containsKey(s)) {
+                sb.append(map.get(s));
+                b = e;
+                e = b + 2;
+            } else {
+                e++;
+            }
+        }
+        return Long.parseLong(sb.toString());
+    }
+}
---
+++ Level.0/src/com/psh/no51_60/no57/Main2.java
@@ -0,0 +1,22 @@
+package com.psh.no51_60.no57;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String numbers = br.readLine();
+        System.out.print(solution(numbers));
+    }
+
+    static long solution(String numbers) {
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < arr.length; i++) {
+            numbers = numbers.replaceAll(arr[i], i+"");
+        }
+        return Long.parseLong(numbers);
+    }
+}
