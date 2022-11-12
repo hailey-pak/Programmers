3cd03d0f3ad6ed654d887e8e67bbacd406cf907c
hailey-pak <haileypak.me@gmail.com>
2022-11-12 13:58:00 +0900

    코딩테스트 입문 > 한 번만 등장한 문자 > Collectors.groupingBy() / filter() / Collectors.joining() 사용

---
+++ Level.0/src/com/psh/no51_60/no59/Main2.java
@@ -0,0 +1,26 @@
+package com.psh.no51_60.no59;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(solution(s));
+    }
+
+    static String solution(String s) {
+        return Arrays.stream(s.split(""))
+                .collect(Collectors.groupingBy(s1 -> s1))
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().size() <= 1)
+                .map(Map.Entry::getKey)
+                .sorted()
+                .collect(Collectors.joining());
+    }
+}
