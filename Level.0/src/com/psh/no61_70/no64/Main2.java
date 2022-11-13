33d77c8476da6b2af481618228b4a30e59bf25be
hailey-pak <haileypak.me@gmail.com>
2022-11-13 21:52:24 +0900

    코딩테스트 입문 > 배열의 유사도 > Arrays.stream() 이용

---
+++ Level.0/src/com/psh/no61_70/no64/Main2.java
@@ -0,0 +1,21 @@
+package com.psh.no61_70.no64;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s1 = br.readLine().split(" ");
+        String[] s2 = br.readLine().split(" ");
+        System.out.println(solution(s1, s2));
+
+    }
+
+    static int solution(String[] s1, String[] s2) {
+        return (int) Arrays.stream(s1).map(s -> Arrays.stream(s2).collect(Collectors.toList()).contains(s)).filter(b -> b).count();
+    }
+}
