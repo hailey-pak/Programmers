86c7693585a65410882f5464a13cba8235e94bc2
hailey-pak <haileypak.me@gmail.com>
2022-11-13 20:54:44 +0900

    코딩테스트 입문 > 가장 큰 수 찾기

---
+++ Level.0/src/com/psh/no61_70/no62/Main.java
@@ -0,0 +1,25 @@
+package com.psh.no61_70.no62;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int[] array = Arrays.stream(str).mapToInt(Integer::parseInt).toArray();
+        System.out.print(Arrays.toString(solution(array)));
+    }
+
+    static int[] solution(int[] array) {
+        int max = Arrays.stream(array).max().getAsInt();
+        int idx = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (max == array[i]) idx = i;
+        }
+        return new int[]{max, idx};
+    }
+}
