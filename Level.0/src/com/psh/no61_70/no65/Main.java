b588b6bc7ad91aa934ece8c38cb69f5abf5e16e1
haileypak <haileypak.me@gmail.com>
2022-11-14 10:37:58 +0900

    코딩테스트 입문 > 숫자 찾기

---
+++ Level.0/src/com/psh/no61_70/no65/Main.java
@@ -0,0 +1,18 @@
+package com.psh.no61_70.no65;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println(solution(num, k));
+    }
+
+    static int solution(int num, int k) {
+        String str = String.valueOf(num);
+        int idx = str.indexOf(k+"");
+        return idx == -1 ? idx : idx + 1;
+    }
+}
