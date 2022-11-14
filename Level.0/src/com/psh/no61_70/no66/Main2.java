81de1966e9273e9160e9f18327e869a18948884a
haileypak <haileypak.me@gmail.com>
2022-11-14 10:47:08 +0900

    코딩테스트 입문 > n의 배수 고르기 > Arrays.stream().filter()이용

---
+++ Level.0/src/com/psh/no61_70/no66/Main2.java
@@ -0,0 +1,19 @@
+package com.psh.no61_70.no66;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] numlist = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        System.out.println(Arrays.toString(solution(n, numlist)));
+    }
+
+    static int[] solution(int n, int[] numlist) {
+        return Arrays.stream(numlist).filter(i -> i % n == 0).toArray();
+    }
+}
