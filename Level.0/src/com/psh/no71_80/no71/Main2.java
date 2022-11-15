727168ee2cc0ca2214b194584426bcc43b543fa4
haileypak <haileypak.me@gmail.com>
2022-11-15 09:02:58 +0900

    코딩테스트 입문 > 세균 증식 > 비트이동연산자 사용

---
+++ Level.0/src/com/psh/no71_80/no71/Main2.java
@@ -0,0 +1,18 @@
+package com.psh.no71_80.no71;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+        System.out.println(solution(n, t));
+    }
+
+    static int solution(int n, int t) {
+        int sum;
+        sum = n << t;
+        return sum;
+    }
+}
