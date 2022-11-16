4d0699fbca287ebc2ad245e904d394f2de71d4ea
haileypak <haileypak.me@gmail.com>
2022-11-16 09:18:25 +0900

    코딩테스트 입문 > 중복된 숫자 개수

---
+++ Level.0/src/com/psh/no71_80/no75/Main.java
@@ -0,0 +1,20 @@
+package com.psh.no71_80.no75;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(solution(array, n));
+    }
+
+    static int solution(int[] array, int n) {
+        return (int) Arrays.stream(array).filter(i -> i == n).count();
+    }
+}
