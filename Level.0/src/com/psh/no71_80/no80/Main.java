11d8b12738e788f6477a3deabbca16dee614fdf0
haileypak <haileypak.me@gmail.com>
2022-11-17 13:03:32 +0900

    코딩테스트 입문 > 다항식 더하기

---
+++ Level.0/src/com/psh/no71_80/no80/Main.java
@@ -0,0 +1,33 @@
+package com.psh.no71_80.no80;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String polynomial = new Scanner(System.in).nextLine();
+        System.out.println(solution(polynomial));
+    }
+
+    static String solution(String polynomial) {
+        String[] str = polynomial.split(" ");
+        int x = 0, y = 0;
+        for (String s : str) {
+            if (s.contains("+")) continue;
+            if (s.contains("x")) {
+                int n = s.equals("x") ? 1 : Integer.parseInt(s.substring(0, s.length() - 1));
+                x += n;
+            } else {
+                y += Integer.parseInt(s);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        if (x != 0) {
+            if (x != 1) sb.append(x);
+            sb.append("x");
+            if (y != 0) sb.append(" + ").append(y);
+        } else {
+            sb.append(y);
+        }
+        return sb.toString();
+    }
+}
