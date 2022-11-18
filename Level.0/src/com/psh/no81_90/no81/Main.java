48c361ea7721f48a092f3fb74e8c401f6f14607e
haileypak <haileypak.me@gmail.com>
2022-11-18 09:20:44 +0900

    코딩테스트 입문 > 숨어있는 숫자의 덧셈 (2)

---
+++ Level.0/src/com/psh/no81_90/no81/Main.java
@@ -0,0 +1,30 @@
+package com.psh.no81_90.no81;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String my_string = new Scanner(System.in).nextLine();
+        System.out.println(solution(my_string));
+    }
+
+    static int solution(String my_string) {
+        int sum = 0;
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if (Character.isDigit(c)) {
+                s.append(c);
+            } else {
+                if (s.toString().trim().length() != 0) {
+                    sum += Integer.parseInt(s.toString());
+                    s = new StringBuilder();
+                }
+            }
+        }
+        if (s.toString().trim().length() != 0) {
+            sum += Integer.parseInt(s.toString());
+        }
+        return sum;
+    }
+}
