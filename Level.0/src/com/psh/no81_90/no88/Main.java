2705bcc5d35d5fb6853b8d174c5baeb8dbcb1e50
hailey-pak <haileypak.me@gmail.com>
2022-11-20 03:13:28 +0900

    코딩테스트 입문 > 유한소수 판별하기

---
+++ Level.0/src/com/psh/no81_90/no88/Main.java
@@ -0,0 +1,36 @@
+package com.psh.no81_90.no88;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(solution(a, b));
+        sc.close();
+    }
+
+    static int solution(int a, int b) {
+        int val = b / gcd(a, b);
+
+        while (val != 1) {
+            if (val % 2 == 0) {
+                val /= 2;
+            } else if (val % 5 == 0) {
+                val /= 5;
+            } else {
+                return 2;
+            }
+        }
+        return 1;
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
