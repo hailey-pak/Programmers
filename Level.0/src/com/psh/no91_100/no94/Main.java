19b4174f081af7ed9180390718623945b9d167d7
haileypak <haileypak.me@gmail.com>
2022-11-22 12:48:39 +0900

    코딩테스트 입문 > 이진수 더하기

---
+++ Level.0/src/com/psh/no91_100/no94/Main.java
@@ -0,0 +1,16 @@
+package com.psh.no91_100.no94;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String bin1 = sc.nextLine();
+        String bin2 = sc.nextLine();
+        System.out.println(solution(bin1, bin2));
+    }
+
+    static String solution(String bin1, String bin2) {
+        return Integer.toBinaryString(Integer.parseInt(bin1, 2) + Integer.parseInt(bin2, 2));
+    }
+}
