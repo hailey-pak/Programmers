d719158063a6946e549abbdaec2fff772a1b5324
hailey-pak <haileypak.me@gmail.com>
2022-11-28 21:27:38 +0900

    코딩테스트 연습 > 스택/큐 > 같은 숫자는 싫어

---
+++ Level.1/src/com/n12906/Main2.java
@@ -0,0 +1,27 @@
+package com.n12906;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        System.out.println(Arrays.toString(solution(arr)));
+    }
+
+    static int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
+
+        for (int a : arr) {
+            if (stack.peek() != a) {
+                stack.push(a);
+            }
+        }
+        return stack.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
