479e2ad8fc08051d4c465d472abf66b3da7bb90b
haileypak <haileypak.me@gmail.com>
2022-11-29 15:14:39 +0900

    코딩테스트 연습 > 연습문제 > 두 정수 사이의 합

---
+++ Level.1/src/com/n12912/Main.java
@@ -0,0 +1,25 @@
+package com.n12912;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(solution(a, b));
+    }
+
+    static long solution(int a, int b) {
+        long answer = 0;
+        if (a > b) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        for (int i = a; i <= b; i++) {
+            answer += i;
+        }
+        return answer;
+    }
+}
