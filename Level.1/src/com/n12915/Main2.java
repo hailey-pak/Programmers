5763b85686e2176db52feb5fe03f222df83a0fff
haileypak <haileypak.me@gmail.com>
2022-11-30 10:58:20 +0900

    코딩테스트 연습 > 연습문제 > 문자열 내 마음대로 정렬하기 > 비교문자열을 StringBuilder 이용 해서 맨앞으로 붙여서 비교 (성능이 더 좋음)

---
+++ Level.1/src/com/n12915/Main2.java
@@ -0,0 +1,28 @@
+package com.n12915;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(Arrays.toString(solution(str, n)));
+    }
+
+    static String[] solution(String[] strings, int n) {
+        for (int i = 0; i < strings.length; i++) {
+            StringBuilder sb = new StringBuilder(strings[i]);
+            strings[i] = sb.insert(0, sb.charAt(n)).toString();
+        }
+
+        Arrays.sort(strings);
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = strings[i].substring(1);
+        }
+        return strings;
+    }
+}
