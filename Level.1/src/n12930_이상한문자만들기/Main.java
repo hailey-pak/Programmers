dddab6c35cf12ff33c6dc14174546aa5bbdc347a
hailey-pak <haileypak.me@gmail.com>
2022-12-03 19:51:25 +0900

    코딩테스트 연습 > 연습문제 > 이상한 문자 만들기

---
+++ Level.1/src/n12930_이상한문자만들기/Main.java
@@ -0,0 +1,30 @@
+package n12930_이상한문자만들기;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = new Scanner(System.in).nextLine();
+        System.out.println(solution(s));
+    }
+
+    static String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        int n = 0;
+        for (char c : s.toCharArray()) {
+            if (c == 32) {
+                sb.append(c);
+                n = 0;
+            } else {
+                if (n % 2 == 0) {
+                    sb.append(Character.toUpperCase(c));
+                } else {
+                    sb.append(Character.toLowerCase(c));
+                }
+                n++;
+            }
+        }
+        return sb.toString();
+    }
+}
