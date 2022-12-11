4317e9e24aae359a8f8e889a86ccb0b0d579f635
hailey-pak <haileypak.me@gmail.com>
2022-12-11 19:25:32 +0900

    코딩테스트 연습 > 코딩테스트 입문 > 직각삼각형 출력하기

--- Level.0/src/com/psh/no21_30/no22/Main.java
+++ Level.0/src/com/psh/no21_30/no22/Main.java
@@ -6,10 +6,7 @@ public class Main {
     public static void main(String[] args) {
         int n = new Scanner(System.in).nextInt();
         for (int i = 1; i < n + 1; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+            System.out.println("*".repeat(i));
         }
     }
 }
