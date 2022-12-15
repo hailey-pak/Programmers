0da026cb78f9be7d851d35046d40d51b7bac7302
haileypak <haileypak.me@gmail.com>
2022-12-15 17:04:35 +0900

    코딩테스트 연습 > 연습문제 > 없는 숫자 더하기

---
+++ Level.1/src/n86051_없는숫자더하기/Main.java
@@ -0,0 +1,23 @@
+package n86051_없는숫자더하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        System.out.println(solution(numbers));
+    }
+
+    static int solution(int[] numbers) {
+        int sum = 45;
+        for (int number : numbers) {
+            sum -= number;
+        }
+        return sum;
+    }
+}
