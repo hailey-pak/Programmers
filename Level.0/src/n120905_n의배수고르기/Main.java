00f75d312297cef9aade46512494008b5dd3356b
hailey-pak <haileypak.me@gmail.com>
2022-12-18 17:38:47 +0900

    코딩테스트 연습 > 코딩테스트 입문 > n의 배수 고르기

--- Level.0/src/n120905_n의배수고르기/Main.java
+++ Level.0/src/n120905_n의배수고르기/Main.java
@@ -1,11 +1,12 @@
-package com.psh.no61_70.no66;
+package n120905_n의배수고르기;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Main2 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -14,6 +15,16 @@ public class Main2 {
     }
 
     static int[] solution(int n, int[] numlist) {
-        return Arrays.stream(numlist).filter(i -> i % n == 0).toArray();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num : numlist) {
+            if (num % n == 0) {
+                list.add(num);
+            }
+        }
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = list.get(i);
+        }
+        return ans;
     }
 }
--- Level.0/src/n120905_n의배수고르기/Main2.java
+++ Level.0/src/n120905_n의배수고르기/Main2.java
@@ -1,13 +1,11 @@
-package com.psh.no61_70.no66;
+package n120905_n의배수고르기;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -16,12 +14,6 @@ public class Main {
     }
 
     static int[] solution(int n, int[] numlist) {
-        List<Integer> list = new ArrayList<>();
-        for (int num : numlist) {
-            if (num % n == 0) {
-                list.add(num);
-            }
-        }
-        return  list.stream().mapToInt(i->i).toArray();
+        return Arrays.stream(numlist).filter(i -> i % n == 0).toArray();
     }
 }
