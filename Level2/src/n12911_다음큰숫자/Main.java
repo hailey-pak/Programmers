bf384f95950b3da622e1f01a8341a4510e38a921
haileypak <haileypak.me@gmail.com>
2023-01-03 10:00:56 +0900

    코딩테스트 연습 > 연습문제 > 다음 큰 숫자

---
+++ Level2/src/n12911_다음큰숫자/Main.java
@@ -0,0 +1,30 @@
+package n12911_다음큰숫자;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = new Scanner(System.in).nextInt();
+        System.out.println(solution(n));
+    }
+
+    static int solution(int n) {
+        int cnt = countOne(Integer.toBinaryString(n));
+
+        int m = n + 1;
+        while (true) {
+            int tmp = countOne(Integer.toBinaryString(m));
+            if (tmp == cnt) break;
+            m++;
+        }
+        return m;
+    }
+
+    static int countOne(String s) {
+        int cnt = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '1') cnt++;
+        }
+        return cnt;
+    }
+}
